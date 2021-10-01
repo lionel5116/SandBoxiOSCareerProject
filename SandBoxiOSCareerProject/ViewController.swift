cd6f24e18513562b7eab421bd41b536cd634e875
lioneljones <lioneljones5116@gmail.com>
2021-10-01 16:07:58 -0500

    Adding this as an initial push to GitHub

--- SandBoxiOSCareerProject/ViewController.swift
+++ SandBoxiOSCareerProject/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //This is the main storyboard view controller
+        print("Starting the iPhone Application")
     }
 
 
