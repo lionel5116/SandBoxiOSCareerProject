decdf52b09d9c6c66d44292c7879a8db9c0b3da7
lioneljones <lioneljones5116@gmail.com>
2021-10-01 23:06:12 -0500

    added travel details controller

---
+++ SandBoxiOSCareerProject/TraveDetailsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TraveDetailsViewController.swift
+//  SandBoxiOSCareerProject
+//
+//  Created by lionel jones on 10/1/21.
+//
+
+import UIKit
+
+class TraveDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Travel Details";
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
