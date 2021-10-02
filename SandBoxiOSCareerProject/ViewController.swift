32bb22068e10aac531d4d29c3652ad3707c973ce
lioneljones <lioneljones5116@gmail.com>
2021-10-01 19:16:23 -0500

    Added a UI Picker control and button logic

--- SandBoxiOSCareerProject/ViewController.swift
+++ SandBoxiOSCareerProject/ViewController.swift
@@ -8,17 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let dataSource = ["Button Image",
+                              "Constraints",
+                              "Background Layout",
+                              "Media Design"]
 
     @IBOutlet weak var btnSelectLesson: UIButton!
     @IBOutlet weak var pkvLessonSelect: UIPickerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //This is the main storyboard view controller
+        
         print("Starting the iPhone Application")
+        pkvLessonSelect.dataSource = self;
+        pkvLessonSelect.delegate = self;
     }
 
 
+}
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataSource.count;
+    }
+    
+    //how many columns
+    func numberOfComponents(in pkvLessonSelect: UIPickerView) -> Int {
+       return 1
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("You selected the lesson \(dataSource[row])");
+        btnSelectLesson.setTitle(dataSource[row],for: .normal);
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        //title for the row in the picker view
+        return dataSource[row]
+    }
 }
 
