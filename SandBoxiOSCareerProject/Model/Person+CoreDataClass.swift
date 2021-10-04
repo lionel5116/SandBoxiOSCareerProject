1e4421dbf68135c63f470d8a729d6c508de6265a
lioneljones <lioneljones5116@gmail.com>
2021-10-04 18:33:55 -0500

    Added my Core Data Model Files

---
+++ SandBoxiOSCareerProject/Model/Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  SandBoxiOSCareerProject
+//
+//  Created by lionel jones on 10/4/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
