db5a750dce6861514c7b02c855ac1b6faeeb2c7d
lioneljones <lioneljones5116@gmail.com>
2021-10-04 18:12:19 -0500

    Added swipe actions to the table view controller

--- SandBoxiOSCareerProject/tableViewControllerExample.swift
+++ SandBoxiOSCareerProject/tableViewControllerExample.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class tableViewControllerExample: UIViewController {
     
@@ -13,7 +14,7 @@ class tableViewControllerExample: UIViewController {
     @IBOutlet weak var btnAddPerson: UIButton!
     
     
-    let contacts = ["Ferron Brown",
+    var contacts = ["Ferron Brown",
                     "Mark Tyson",
                     "Dac Black",
                     "Rick James"]
@@ -31,6 +32,50 @@ class tableViewControllerExample: UIViewController {
     @IBAction func acAddPerson(_ sender: Any) {
         print("You added a person");
     }
+    
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+   
+    
+    /*BELOW ARE SWIPE ACTIONS*/
+    /*IOS 15 USES CONTEXTUAL ACTIONS - USE THIS TO NOT HAVE WARNINGS!!!  */
+    /*DELETE*/
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+           return UISwipeActionsConfiguration(actions: [
+               makeDeleteContextualAction(forRowAt: indexPath)
+           ])
+       }
+    
+    private func makeDeleteContextualAction(forRowAt indexPath: IndexPath) -> UIContextualAction {
+            return UIContextualAction(style: .destructive, title: "Delete") { (action, swipeButtonView, completion) in
+                print("DELETE HERE")
+                self.contacts.remove(at: indexPath.row);
+                self.tableView.deleteRows(at: [indexPath], with: .automatic);
+
+                completion(true)
+            }
+    }
+    
+    /*UPDATE*/
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+           return UISwipeActionsConfiguration(actions: [
+            makeUpdateContextualAction(forRowAt: indexPath)
+           ])
+       }
+    
+    private func makeUpdateContextualAction(forRowAt indexPath: IndexPath) -> UIContextualAction {
+        return UIContextualAction(style: .normal, title: "Update") { (action, swipeButtonView, completion) in
+                print("Updating Here")
+                
+                print("Updating somethig here....!!")
+
+                completion(true)
+            }
+    }
+    
+    
 }
 
 
@@ -39,6 +84,9 @@ extension tableViewControllerExample: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("you tapped me")
     }
+    
+    
+    
 }
 
 extension tableViewControllerExample: UITableViewDataSource{
@@ -59,5 +107,7 @@ extension tableViewControllerExample: UITableViewDataSource{
         return cell;
     }
 }
+
+
 
 
